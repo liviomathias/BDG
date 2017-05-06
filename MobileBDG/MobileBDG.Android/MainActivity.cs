95ff5b485ce07c0f035b4b6c9b52a74694fa6f59
Sara Honorato <sarahonorato@msn.com>
2017-05-06 15:40:53 -0700

    Splash Screen

--- MobileBDG/MobileBDG.Android/MainActivity.cs
+++ MobileBDG/MobileBDG.Android/MainActivity.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace MobileBDG.Droid
 {
-	[Activity (Label = "MobileBDG", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity (Label = "BDG", Icon = "@drawable/icon", Theme="@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
@@ -20,7 +16,7 @@ namespace MobileBDG.Droid
 			base.OnCreate (bundle);
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication (new MobileBDG.App ());
+			LoadApplication (new App());
 		}
 	}
 }
---
+++ MobileBDG/MobileBDG.Android/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Content.PM;
+
+namespace MobileBDG.Droid
+{
+    [Activity(Label = "BDG", Theme = "@style/Theme.Splash", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+            Finish();
+        }
+    }
+}
